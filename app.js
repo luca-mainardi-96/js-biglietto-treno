7a9333f424ee3488b9eb8711fb271d7394fcaf14
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-05-10 18:13:15 +0200

    inizio

---
+++ app.js
@@ -0,0 +1,24 @@
+//definire domande
+
+const tratta = parseFloat(prompt("Quanti km devi percorrere?45") )
+
+console.log(tratta)
+
+const eta = parseFloat(prompt("quanti anni hai?"))
+
+console.log(eta)
+
+//definire prezzo
+
+const prezzo = tratta*0.21
+
+console.log(prezzo)
+
+
+
+
+//definire sconto
+    //sconto 20% minorenni15
+    //sconto 40% over 65
+
+//prezzo finale156
